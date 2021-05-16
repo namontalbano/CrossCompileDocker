4220d69aa303d7ecefd64c9a27d98797f81fe340
nicholas montalbnao <namontalbano@email.neit.edu>
2021-05-16 11:09:38 -0400

    arm-eabi: intial commit

---
+++ arm-eabi/build_scripts/build_gcc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+GCC_VERSION=$1
+
+cd $PREFIX/src
+mkdir build-gcc
+cd build-gcc
+../gcc-${GCC_VERSION}/configure --target="$TARGET" --prefix=$PREFIX --disable-nls --enable-languages=c,c++ --without-headers
+make all-gcc
+make all-target-libgcc
+make install-gcc
+make install-target-libgcc
+
+cd $PREFIX/src
+rm -rf build-gcc.sh build-gcc gcc-${GCC_VERSION}
